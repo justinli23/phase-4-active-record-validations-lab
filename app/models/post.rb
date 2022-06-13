1a2cb76511324b7cbf09590e56fa0330d6e9f052
Justin Li <justinlit23@gmail.com>
2022-06-12 20:25:00 -0700

    commit tree 4aca6b8a2e617eb0fd2c16923df75f68b7ba6627

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: :true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :clickbait_check
+
+    def clickbait_check
+        if title.present?
+            unless title.include?("Won't Believe")
+                errors.add(:title)
+            end
+        end
+    end
 end
